5166db04cd719b0e82afe1fe54aaf0c1c9b2de92
Mr.Puttipong Doungvichai <38111511+theputtipong@users.noreply.github.com>
2024-01-07 03:53:46 +0700

    up

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = 'your_email@example.com'
 app.config['MAIL_PASSWORD'] = 'your_email_password'
+app.config['FLASK_ENV'] = 'production'
 
 mail = Mail(app)
 
@@ -27,17 +28,17 @@ def download_file(filename):
 
 @app.route('/send_email', methods=['POST'])
 def send_email():
-    #from input
-    name = request.form['name']
-    email = request.form['email']
-    message = request.form['message']
+    # #from input
+    # name = request.form['name']
+    # email = request.form['email']
+    # message = request.form['message']
 
-    subject = f'Flask Contact Form {email}'
-    body = f'Name: {name}\nEmail: {email}\nMessage: {message}'
+    # subject = f'Flask Contact Form {email}'
+    # body = f'Name: {name}\nEmail: {email}\nMessage: {message}'
 
-    msg = Message(subject, recipients=email)
-    msg.body = body
-    mail.send(msg)
+    # msg = Message(subject, recipients=email)
+    # msg.body = body
+    # mail.send(msg)
     return 'Email sent successfully!'
 
 if __name__ == '__main__':
